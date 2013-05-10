2065827e516d8d94ef6bd135660d36f8f969d6f4
Jesse Jurman <jrjurman@gmail.com>
2013-05-09 22:25:25 -0400

    Using Symbols, and added board methods

--- Board.rb
+++ Board.rb
@@ -1,19 +1,49 @@
 require "colorize"
 require "./Tile.rb"
 
+# Board Class, defines a scrabble board on which several tiles exist
 class Board
   attr_accessor :board
 
+  # Creates a new board, reads in from a txt file
+  # and generates a 2 dimensional array
   def initialize( boardFile )
     @board = [[]]
     f = File.open( boardFile )
     f.each_char do |c|
-      c != "\n" ? @board[@board.size()-1] << c : @board << []
+      c != "\n" ? @board[@board.size()-1] << Tile.new(" ", c) : @board << []
     end
   end
 
-  def showBoard
-    board.each do |tile|
+  # Writes a character to the board
+  # x, y = (1..15)
+  # c = (A..Z, a..z)
+  def write_char( x, y, c )
+    @board[x][y] = Tile.new(c, "")
+  end
+
+  # Writes a line on the board
+  # direction = (:left | :down)
+  def write_board( x, y, direction, line )
+
+  end
+
+  # Prints the board to STDOUT
+  def show_board
+    @board.each do |tileSet|
+      tileSet.each do |tile|
+        print(" #{tile.show_tile}")
+      end
+      print(" \n")
     end
   end
+
 end
+
+#test run
+b = Board.new( "board.txt" )
+b.write_char( 7, 7, "f" )
+b.write_char( 7, 8, "a" )
+b.write_char( 7, 9, "c" )
+b.write_char( 7, 10,"e" )
+b.show_board
--- Tile.rb
+++ Tile.rb
@@ -1,10 +1,40 @@
-class Tiles
+require "colorize"
+
+class Tile
 
   attr_accessor :letter, :multiplier
 
-  def initialize( multiplier )
-    @multiplier = multiplier
-    @letter = " "
+  def initialize( letter, multiplier )
+    @letter = letter
+    case multiplier
+    when "T"
+      @multiplier = :T
+    when "D"
+      @multiplier = :D
+    when "t"
+      @multiplier = :t
+    when "d"
+      @multiplier = :d
+    else
+      @multiplier = :none
+    end
+  end
+
+  def show_tile
+    case @multiplier
+    when :T
+      pLetter = "#".red
+    when :D
+      pLetter = "#".yellow
+    when :t
+      pLetter = "#".blue
+    when :d
+      pLetter = "#".cyan
+    else
+      pLetter = @letter
+    end
+    
+    return pLetter
   end
 
 end
