568d6c91c70f79c6905576a798b3e434fb3cb91d
Jesse Jurman <jrjurman@gmail.com>
2013-05-09 20:07:12 -0400

    moved Tile, Board now adds txt files correctly

--- Board.rb
+++ Board.rb
@@ -1,10 +1,19 @@
 require "colorize"
+require "./Tile.rb"
 
 class Board
   attr_accessor :board
 
-  def initialize
-    board = Hash()
+  def initialize( boardFile )
+    @board = [[]]
+    f = File.open( boardFile )
+    f.each_char do |c|
+      c != "\n" ? @board[@board.size()-1] << c : @board << []
+    end
+  end
 
+  def showBoard
+    board.each do |tile|
+    end
   end
 end
--- Tile.rb
+++ Tile.rb
@@ -3,7 +3,8 @@ class Tiles
   attr_accessor :letter, :multiplier
 
   def initialize( multiplier )
-
+    @multiplier = multiplier
+    @letter = " "
   end
 
 end
